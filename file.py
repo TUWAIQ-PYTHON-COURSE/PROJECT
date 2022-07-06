9ea44c9dc77b5f1adcdc6d1e22373ddc4b3ee53a
hashem-ali <hashem.bajabaa1@gmail.com>
2022-07-06 03:17:41 +0300

    Delete all unneeded comments and code

--- file.py
+++ file.py
@@ -9,9 +9,8 @@ def create_file():
     try:
         f = open(file_name+".txt", "x")
         f.close()
-    except FileExistsError:
-       # print("the file is already created no need to create again")
-       return
+    except FileExistsError as e:
+       return print(e)
 def write_file(name : str, value : str):
     create_file()
     # Open the file in append & read mode ('a+')
@@ -41,29 +40,15 @@ def read_by_key_file(key : str):
         my_list = list()
         with open(file_name+".txt", "r") as f:
             my_list = f.readlines()
-            #print(my_list)
             for element in my_list:
                 line = my_list[x]
-                #print(line.split(" ").pop(0))
                 word = line.split(" ").pop(0)
-                #print(word)
-                #print(key)
                 x = x + 1
                 if key.casefold() == word.casefold():
                     return  line.split(" ").pop(2)
                 else:
                     continue
-            # for i in f.readlines():
-            #     f.seek(0)
-                #word = f.readlines()[0].split(" ").pop(0)
-                
-                # word = line. split(' '). pop(0)
-                #print(word)
-                # if word == key:
-                #     return True
-                # else:
-                #     return False
-        
+       
         return False
 
 def check_key(key : str) -> bool:
@@ -71,46 +56,14 @@ def check_key(key : str) -> bool:
     my_list = list()
     with open(file_name+".txt", "r") as f:
         my_list = f.readlines()
-        #print(my_list)
         for element in my_list:
             line = my_list[x]
-          #  print(line.split(" ").pop(0))
             word = line.split(" ").pop(0)
-           # print(word)
-            #print(key)
             x = x + 1
             if key.casefold() == word.casefold():
                 return True
             else:
                  continue
-        # for i in f.readlines():
-        #     f.seek(0)
-            #word = f.readlines()[0].split(" ").pop(0)
-            
-            # word = line. split(' '). pop(0)
-            #print(word)
-            # if word == key:
-            #     return True
-            # else:
-            #     return False
-    
+       
     return False
         
-        
-# print(check_key("gmail"))
-
-# f = open("user_passwords.txt", "r")
-# print(f.readlines()[0])
-# x = f.readlines()
-# print(x)
-# if f.readlines().__eq__("k"):
-#     print("True")
-
-# word2 = "hashem"
-# word1 = "Hashem"
-# if word2.casefold() == word1.casefold():
-#     print("equal")
-
-# print(read_by_key_file("spotify"))
-
-# i = int(
